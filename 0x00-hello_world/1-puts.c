189c96b0e214bd0d7b6c3f826687609bb540a7e1
Pugianf <pugianf@gmail.com>
2021-04-09 00:35:22 +0000

    fooooooi

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-/* explicacao qualquer */ int main(void)
+/*
+ *explicacao qualquer */int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
