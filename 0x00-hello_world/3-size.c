d59c9a860abffc44ae1d6e9b191356c0f18a433a
Pugianf <pugianf@gmail.com>
2021-04-09 01:30:02 +0000

    sera?

--- 0x00-hello_world/3-size.c
+++ 0x00-hello_world/3-size.c
@@ -7,5 +7,5 @@ printf("Size of an int:%2d byte(s)\n", sizeof(int));
 printf("Size of a long int:%2d byte(s)\n", sizeof(long int));
 printf("Size of a long long int:%2d byte(s)\n", sizeof(long long int));
 printf("Size of a float:%2d byte(s)\n", sizeof(float));
-return (0);
+return(0);
 }
