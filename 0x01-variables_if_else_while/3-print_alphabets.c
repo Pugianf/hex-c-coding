d24d6df20d915f0a7b7588276494388a8ad6e8e5
Pugianf <pugianf@gmail.com>
2021-04-10 14:25:32 -0300

    1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * explicacao aqui*/int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for putchar (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
