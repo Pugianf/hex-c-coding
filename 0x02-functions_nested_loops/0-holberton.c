7f8247dd82cf596944ecebdefc1cf394fcb91f76
Pugianf <pugianf@gmail.com>
2021-04-17 08:59:57 -0300

    update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,23 @@
-#include <holberton.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0, success
+ */
 
-/*
- * explicacao aqui*/int main(void)
+#include "holberton.h"
+
+int main(void)
 {
-char *c;
-c = ("Holberton");
-_putchar(c);
-return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	
+	return (0);
 }
