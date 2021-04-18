43547486c795c1610b06665a09bf2d29c8f31c7c
Pugianf <pugianf@gmail.com>
2021-04-18 16:20:49 -0300

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,12 @@
 
 #include "holberton.h"
 
+int fork(int c);
+
 void times_table(void)
 {
 	int a, b, c;
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
@@ -17,31 +20,48 @@ void times_table(void)
 
 			if ( b == 0)
 			{
-				_putchar('0'); _putchar(44); _putchar(32);
+				_putchar('0');
+				_putchar(44);
+				_putchar(32);
 			}
 			else if ( b < 9)
 			{
-				if (c < 10)
-				{
-					_putchar(32); _putchar(c + '0'); _putchar(44); _putchar(32);
-				}
-				else
-				{
-					_putchar((c / 10) + '0'); _putchar((c % 10) + '0'); _putchar(44); _putchar(32);
-				}
+				fork(c);	
 			}
 			else
 			{
 				if (c < 10)
 				{
-					_putchar(32); _putchar(c + '0'); _putchar('\n');
+					_putchar(32);
+					_putchar(c + '0');
+					_putchar('\n');
 				}
 				else
 				{
-					_putchar((c / 10) + '0'); _putchar((c % 10) + '0'); _putchar('\n');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					_putchar('\n');
 				}
 			}
 		}
 		b = 0;
+	}
+}
+
+void fork(int c)
+{
+	if (c < 10)
+	{
+		_putchar(32);
+		_putchar(c + '0');
+		_putchar(44);
+		_putchar(32);
+	}
+	else
+	{
+		_putchar((c / 10) + '0');
+		_putchar((c % 10) + '0');
+		_putchar(44);
+		_putchar(32);
 	}
 }
