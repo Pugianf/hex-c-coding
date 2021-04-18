abb09920ebb2d14e63bb97b0834223fbc7fc82ad
Pugianf <pugianf@gmail.com>
2021-04-18 16:03:50 -0300

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+/**
+ * times_table - return the 9 times table
+ *
+ * Return: void
+ */
+
+#include "holberton.h"
+
+int times_table(void)
+{
+	int a, b, c;
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = (a * b);
+
+			if ( b == 0)
+			{
+				_putchar('0'); _putchar(44); _putchar(32);
+			}
+			else if ( b < 9)
+			{
+				if (c < 10)
+				{
+					_putchar(32); _putchar(c + '0'); _putchar(44); _putchar(32);
+				}
+				else
+				{
+					_putchar((c / 10) + '0'); _putchar((c % 10) + '0'); _putchar(44); _putchar(32);
+				}
+			}
+			else
+			{
+				if (c < 10)
+				{
+					_putchar(32); _putchar(c + '0'); _putchar('\n');
+				}
+				else
+				{
+					_putchar((c / 10) + '0'); _putchar((c % 10) + '0'); _putchar('\n');
+				}
+			}
+		}
+		b = 0;
+	}
+}
