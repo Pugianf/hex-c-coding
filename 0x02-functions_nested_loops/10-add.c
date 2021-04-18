d74f11d4ad948ac5886d0393058581802a6b5fa3
Pugianf <pugianf@gmail.com>
2021-04-18 19:14:04 -0300

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - a function that adds 2 integers
+ *
+ * Return: the sum of the 2 arguments
+ */
+
+#include "holberton.h"
+
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+	return (c);
+}
