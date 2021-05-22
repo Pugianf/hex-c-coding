16dc077eef20bd916e97f08dbe2d81b40c70a63a
Pugianf <pugianf@gmail.com>
2021-05-22 15:29:40 -0300

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints n times '_', then creating a line on the screen
+ *
+ * @n: number of times that '_' must be printed
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = n; a > 0; a--)
+		{
+			_putchar(95);
+		}
+	}
+
+	_putchar('\n');
+}
