5d7798e436d1e460b2dee1133dcd57dd0af337df
Pugianf <pugianf@gmail.com>
2021-05-22 16:33:46 -0300

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - creates a diagonal with backlashs and spaces
+ *
+ * @n: number of diagonals that must be created
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int a = 1;
+
+	if (n > 0)
+	{
+		while (a < n );
+		{
+			for (i = 1; i = a; i++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+			a++;
+		}
+	}
+}
