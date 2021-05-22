a571b442cc270f5c792d992709e148929830f0de
Pugianf <pugianf@gmail.com>
2021-05-22 13:54:42 -0300

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints from 0 to 9, but 2 and 4
+ *
+ * Return: the sequence printed
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 || c != 52)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void print_most_numbers(void);
 int _putchar(char c);
 void print_numbers(void);
 int mul(int a, int b);
