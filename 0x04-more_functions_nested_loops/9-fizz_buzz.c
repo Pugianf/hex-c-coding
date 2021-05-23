d35f7d9e01ac9b24c7ab598bc70aead234c59430
Pugianf <pugianf@gmail.com>
2021-05-23 11:42:21 -0300

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - ignores multiples of 3 and 5 with fizz and buzz
+ */
+
+void fizz_buzz(void)
+{
+	int i = 1;
+
+	for (; i < 101; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			if ((i % 5) == 0)
+			{
+				printf("FizzBuzz\n");
+			}
+			else
+			{
+				printf("Fizz\n");
+			}
+		else
+		{
+			if ((i % 5) == 0)
+			{
+				printf("Buzz\n");
+			}
+			else
+			{
+				printf("%i\n", i);
+			}
+}
