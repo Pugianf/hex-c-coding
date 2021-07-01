11b712623f30b4aa83f6dd29995af0ef13420632
Pugianf <pugianf@gmail.com>
2021-06-30 21:55:15 -0300

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
 
+/**
+ * reset_to_98 - updates the value of a pointer to 98
+ *
+ * Return: the pointer's value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
