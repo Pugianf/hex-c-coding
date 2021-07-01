0fb0130275db3dcbc6cf989928938d56876486b8
Pugianf <pugianf@gmail.com>
2021-06-30 22:10:39 -0300

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,12 @@
 /**
  * reset_to_98 - updates the value of a pointer to 98
  *
+ * @*n: the pointer as argument
+ *
  * Return: the pointer's value
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
