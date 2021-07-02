755c9f395d4f471ad0d0b109f4d5d586d5f82db7
Pugianf <pugianf@gmail.com>
2021-07-02 20:46:32 -0300

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: argument input
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	if ((i - 1) % 2) == 0
+	{
+		i = (i - 2);
+	}
+	else
+	{
+		i = (i - 1);
+	}
+	
+	for (j = 0; j <= i; 2)
+	{
+		_putchar(*(str + j));
+	}
+	_putchar(10);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);
 void _puts(char *str);
