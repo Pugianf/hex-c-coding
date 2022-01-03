9d3a2a69d29527b80e3a336503d5d08210394ba9
Pugianf <pugianf@gmail.com>
2022-01-03 16:22:41 -0300

    update

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+/**
+ * _strncpy - function that copies a string
+ *
+ * @dest: pointer to a char
+ *
+ * @src: pointer to a char
+ *
+ * @n: integer
+ *
+ * Return: dest char
+ */
+
+#include "holberton.h""
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	for (; i < n && *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	*(dest + i + 1) = '\0';
+
+	return (dest);
+}
