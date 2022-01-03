f1ef7e302a531ab7d4dad9d1e5bd971fc5e4c9d4
Pugianf <pugianf@gmail.com>
2022-01-03 15:47:56 -0300

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+/**
+ * cap_string - function that capitalizes all words of a str
+ *
+ * @s: given string
+ *
+ * Return: string with first char captalized
+ */
+
+char *cap_string(char *S)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] == 44 || s[i] == 59 || s[i] == 46 || s[i] == 33 || s[i] == 63 || s[i] == 34 || s[i] == 40 || s[i] == 41 || s[i] == 123 || s[i] == 125 || i == 0)
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] = s[i] + 32;
+		}
+	}
+
+	return (s)
+}
