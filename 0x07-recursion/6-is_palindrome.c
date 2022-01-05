fa2e986c0d19dce3aa6e3b4dde01ff2857c206cb
Pugianf <pugianf@gmail.com>
2022-01-05 18:35:01 -0300

    update

--- 0x07-recursion/6-is_palindrome.c
+++ 0x07-recursion/6-is_palindrome.c
@@ -1,15 +1,64 @@
 /**
- * is_palindrome - returns 1 if a str is a palindrome and 0 if not
- *
- * @s: string to be tested
- *
- * Return: 1 for palinfrome and 0 if not
+ * is_palindrome - 1 if the input is a prime num, otherwise 0.
+ * lengthPal - recursive function to calculate length.
+ * checkPal - recursive function for palindrome.
+ * @len: integer number
+ * @i: integer
+ * @s: pointer to char
+ * Return: integer
  */
+
+#include "holberton.h"
+
+int checkPal(char *s, int i, int len);
+int lengthPal(char *s);
 
 int is_palindrome(char *s)
 {
-	int i;
+	int len, i;
+
+	len = lengthPal(s);
 
 	i = 0;
+	if (len == 0)
+	{
+		return (1);
+	}
+	return (checkPal(s, i, len - 1));
+}
 
+/**
+ * lengthPal - recursive function to calculate length.
+ * @s: pointer to char
+ * Return: integer
+ */
 
+int lengthPal(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + lengthPal(s + 1));
+}
+
+/**
+ * checkPal - recursive function for palindrome.
+ * @len: integer number
+ * @i: integer
+ * @s: pointer to char
+ * Return: integer
+ */
+
+int checkPal(char *s, int i, int len)
+{
+	if (s[i] != s[len])
+	{
+		return (0);
+	}
+	if ((i == len) || (i + 1) == len)
+	{
+		return (1);
+	}
+	return (checkPal(s, i + 1, len - 1));
+}
