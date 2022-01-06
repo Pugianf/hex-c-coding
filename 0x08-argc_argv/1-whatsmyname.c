feaecd3e5ecf541e2fa3ca0c846f82dd7e06d538
Pugianf <pugianf@gmail.com>
2022-01-05 23:18:12 -0300

    update

---
+++ 0x08-argc_argv/1-whatsmyname.c
@@ -0,0 +1,15 @@
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * Return: name of the program
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf(*argv[0]);
+
+	return (0);
+}
---
+++ 0x08-argc_argv/README.md
@@ -0,0 +1,7 @@
+<h2>Holberton School - C Coding</h2>
+
+<h3>C - More pointers, arrays and strings</h3>
+
+---
+
+Practicing more pointers, arrays and strings in C!
