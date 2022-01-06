518bfcb44dc09252506133c32336db0de4caf945
Pugianf <pugianf@gmail.com>
2022-01-06 09:48:45 -0300

    update

---
+++ 0x08-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+/**
+ * main - main function
+ *
+ * @argc: arguments count
+ *
+ * @argv: arguments vector
+ *
+ * Return: numbers added
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argc[])
+{
+	int i, j = 0;
+
+	if (arg c < 2)
+	{
+		printf("0\n");
+	}
+	else if
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (atoi(argv[i] == False))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				j += argv[i];
+			}
+		}
+		return (0);
+	}
+}
