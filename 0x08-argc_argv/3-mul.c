6e91884aaf0ac6533cca5373751ee2ec7cd72066
Pugianf <pugianf@gmail.com>
2022-01-06 09:34:15 -0300

    update

--- 0x08-argc_argv/3-mul.c
+++ 0x08-argc_argv/3-mul.c
@@ -10,6 +10,7 @@
 
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		i = atoi(argv[1]i);
+		i = atoi(argv[1]);
 		j = atoi(argv[2]);
 		k = (i * j);
 
