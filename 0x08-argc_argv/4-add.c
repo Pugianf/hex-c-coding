1df545d4695b0ea8a94ae4e792fb8f45e275ec48
Pugianf <pugianf@gmail.com>
2022-01-06 10:13:40 -0300

    update

--- 0x08-argc_argv/4-add.c
+++ 0x08-argc_argv/4-add.c
@@ -1,6 +1,10 @@
 /**
  * main - main function
  *
+ * intlen- recursive to calculate digits in number
+ *
+ * @c: integer
+ *
  * @argc: arguments count
  *
  * @argv: arguments vector
@@ -8,33 +12,50 @@
  * Return: numbers added
  */
 
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+int intlen(int c);
+int main(int argc, char **argv)
 {
-	int i, j = 0;
+	int result, i, a, b;
+
+	result = 0;
 
 	if (argc < 2)
 	{
 		printf("0\n");
-	}
-	else
-	{
-		for (i = 0; i < argc; i++)
-		{
-			if (__builtin_types_compatible_p(typeof(atoi(argv[i]), int)))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				j += atoi(argv[i]);
-			}
-		}
-		printf("d\n", j);
 		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		a = strlen(argv[i]);
+		b = intlen(atoi(argv[i]));
+
+		if (a != b)
+		{
+			printf("Error\n");
+			return (0);
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
+
+/**
+ * intlen - recursive to calculate digits in number
+ * @c: integer
+ * Return: integer
+ */
+
+int intlen(int c)
+{
+	if (c == 0)
+	{
+		return (0);
+	}
+	return (1 + intlen(c / 10));
 }
