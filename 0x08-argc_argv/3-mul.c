1a3a410f88592d566a38522a5409c3e517c5bb59
Pugianf <pugianf@gmail.com>
2022-01-06 08:50:04 -0300

    update

---
+++ 0x08-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+/**
+ * main - main function
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: arguments multiplied
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2 && argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", argv[2] * argv[3]);
+		return (0);
+	}
+}
