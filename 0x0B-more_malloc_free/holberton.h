e593a647f02b30162ff74e3e567e0b91f164a165
Pugianf <pugianf@gmail.com>
2022-01-10 11:14:12 -0300

    update

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -30,5 +30,8 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif /* HOLBERTON_H */
