321599eb40555699bbd7a304f07e5d752ae37174
Pugianf <pugianf@gmail.com>
2022-01-09 23:06:29 -0300

    update

---
+++ 0x09-malloc_free/5-argstostr.c
@@ -0,0 +1,56 @@
+/**
+ * argstostr - function that concatenates all arguments of the program
+ * ac: integer
+ * av: **char
+ * Return: char pointer
+ * srleng - string length
+ * @str: arguments char
+ */
+
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int srleng(char *str);
+
+char *argstostr(int ac, char **av)
+{
+	char *arr;
+	int i, j, b;
+	int len;
+	int leng;
+
+	len = 0;
+
+	if ((ac == 0) || (av == NULL))
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < ac; i++)
+		{
+			j = 0;
+			while (av[i][j] != '\0')
+			{
+				j++;
+
+			}
+			len += j;
+		}
+
+		arr = (char *)malloc(sizeof(char) * (len + ac) + 1);
+
+		b = 0;
+		for (i = 0; i < ac; i++)
+		{
+			leng = srleng(av[i]);
+			for (j = 0; j < leng; j++)
+			{
+				arr[b] = av[i][j];
+				b++;
+			}
+			arr[b] = '\n';
+			b++;
+		}
+
