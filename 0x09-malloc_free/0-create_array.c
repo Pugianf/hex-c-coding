46317c257a8fe03707e4af46900c2fe0319bb716
Pugianf <pugianf@gmail.com>
2022-01-09 22:48:09 -0300

    update

--- 0x09-malloc_free/0-create_array.c
+++ 0x09-malloc_free/0-create_array.c
@@ -13,10 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *s = NULL;
 	unsigned int i;
 
-	if ((size == 0) || (s == NULL))
+	if (size == 0)
 		return (NULL);
 
 	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		*(s + i) = c;
